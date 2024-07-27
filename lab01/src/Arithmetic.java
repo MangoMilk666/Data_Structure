27b8b61532f493deafa0ee8c9020693a283bce61
MangoMilk666 <2767620910@qq.com>
2024-07-27 23:34:07 +0800

    lab01: Completed Arithmetic.java

--- lab01/src/Arithmetic.java
+++ lab01/src/Arithmetic.java
@@ -12,7 +12,7 @@ public class Arithmetic {
      * @return Product of a and b
      * */
     public static int product(int a, int b) {
-        return a * b;
+        return a + b;
     }
 
     /** Computes sum of two ints (incorrectly).
