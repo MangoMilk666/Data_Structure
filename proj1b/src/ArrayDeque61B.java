f17a9867d42cc752fd1821cde51b3cab1bfe6196
MangoMilk666 <2767620910@qq.com>
2024-09-24 10:51:14 +0800

    Unfinished code for CS61B proj1b

--- proj1b/src/ArrayDeque61B.java
+++ proj1b/src/ArrayDeque61B.java
@@ -1,2 +1,136 @@
-package PACKAGE_NAME;public class ArrayDeque61B {
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayDeque61B<T> implements Deque61B<T> {
+    private T[] items;
+    private int size;
+    //the front and back indice
+    private int front;
+    private int back;
+
+    //empty constructor
+    public ArrayDeque61B() {
+        items = (T[]) new Object[8];
+        size = 0;
+        front = 0;
+        back = -1;
+        //initialize back with -1 to avoid that position 0 could be taken up
+        // if using addLast to empty lists
+    }
+
+    @Override
+    public void addFirst(T x) {
+        if (size == items.length) {
+            resize(items.length * 2);
+        }
+        //头部添加，则front索引减1再求余，如此则可回溯到末端
+        front = (front - 1 + items.length) % items.length;
+        items[front] = x;
+        size++;
+        if (size == 1) {
+            back = front;
+        }
+    }
+
+    @Override
+    public void addLast(T x) {
+        if (size == items.length) {
+            resize(size * 2);
+        }
+        back = (back + 1 + items.length) % items.length;
+        items[back] = x;
+        size++;
+    }
+
+    @Override
+    public List<T> toList() {
+        List<T> returnList = new ArrayList<>();
+        for (int i=0; i < size; i++){
+            //add添加对应元素到克隆数组末尾
+            returnList.add(items[(front + i) % items.length]);
+        }
+        return returnList;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        if (size == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public T removeFirst() {
+        if (items[front] == null || size == 0) {
+            return null;
+        }
+        //resize down if necessary
+        if (items.length > 15 && size*1.0/items.length < 0.25){
+            resize(items.length/2);
+        }
+        T removed = items[front];
+        //移除处元素赋值为null
+        items[front] = null;
+        front = (front + 1) % items.length;
+        size--;
+        return removed;
+    }
+
+    @Override
+    public T removeLast() {
+        if (items[back] == null || size == 0) {
+            return null;
+        }
+
+        //resize down if necessary
+        if (items.length > 15 && size*1.0/items.length < 0.25){
+            resize(items.length/2);
+        }
+
+        T removed = items[back];
+        //重置为null
+        items[back] = null;
+        back = (back - 1 + items.length) % items.length;
+        size--;
+        return removed;
+    }
+
+
+    @Override
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            return null;
+        }
+        return items[(front + index) % items.length];
+    }
+
+
+    @Override
+    public T getRecursive(int index) {
+        throw new UnsupportedOperationException("No need to implement getRecursive for proj 1b");
+    }
+
+    private void resize(int capacity) {
+        T[] newItems = (T[]) new Object[capacity];
+        /*
+        将老数组元素通过遍历逐个复制到新数组，不使用arraycopy
+        System.arraycopy(items, 0, newItems, 0, size);
+        items = newItems;
+         */
+        for (int i = 0; i < size; i++) {
+            newItems[i] = items[(front + i) % items.length];
+        }
+        //将newItems设为数组主体。并重置front和back
+        items = newItems;
+        front = 0;
+        //因为此时是按照顺序复制了原数组元素，所以front, back分别为前端和末端索引
+        back = size-1;
+    }
 }
+
--- proj1b/tests/ArrayDeque61BTest.java
+++ proj1b/tests/ArrayDeque61BTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Field;
+import java.util.Arrays;
 import java.util.List;
 
 import static com.google.common.truth.Truth.assertThat;
@@ -10,14 +11,177 @@ import static com.google.common.truth.Truth.assertWithMessage;
 
 public class ArrayDeque61BTest {
 
-//     @Test
-//     @DisplayName("ArrayDeque61B has no fields besides backing array and primitives")
-//     void noNonTrivialFields() {
-//         List<Field> badFields = Reflection.getFields(ArrayDeque61B.class)
-//                 .filter(f -> !(f.getType().isPrimitive() || f.getType().equals(Object[].class) || f.isSynthetic()))
-//                 .toList();
-//
-//         assertWithMessage("Found fields that are not array or primitives").that(badFields).isEmpty();
-//     }
+     @Test
+     @DisplayName("ArrayDeque61B has no fields besides backing array and primitives")
+     void noNonTrivialFields() {
+         List<Field> badFields = Reflection.getFields(ArrayDeque61B.class)
+                 .filter(f -> !(f.getType().isPrimitive() || f.getType().equals(Object[].class) || f.isSynthetic()))
+                 .toList();
 
+         assertWithMessage("Found fields that are not array or primitives").that(badFields).isEmpty();
+     }
+     //if error occurs with null, there might be offset error in an Arraylist
+     @Test
+    public void addFirstTest(){
+         ArrayDeque61B<Integer> al1 = new ArrayDeque61B<>();
+         //测试空顺序表
+         al1.addFirst(1);
+         assertThat(al1.toList()).containsExactly(1).inOrder();
+         //测试remove后继续addFirst
+         al1.removeLast();
+         al1.addFirst(1);
+
+         //测试非空顺序表
+         al1.addFirst(2);
+         al1.addFirst(3);
+         al1.addFirst(4);
+         al1.addFirst(5);
+         al1.addFirst(6);
+         al1.addFirst(7);
+         al1.addFirst(8);
+         assertThat(al1.toList()).containsExactly(8,7,6,5,4,3,2,1).inOrder();
+
+         //测试full_trigger_resize顺序表
+         al1.addFirst(9);
+         assertThat(al1.toList()).containsExactlyElementsIn(Arrays.asList(9,8,7,6,5,4,3,2,1));
+
+     }
+
+    @Test
+    public void addLastTest(){
+        ArrayDeque61B<Integer> al1 = new ArrayDeque61B<>();
+        //测试空顺序表
+        al1.addLast(1);
+        assertThat(al1.toList()).containsExactly(1).inOrder();
+        //测试remove后继续addLast
+        al1.removeLast();
+        al1.addLast(1);
+
+        //测试非空顺序表
+        al1.addLast(2);
+        al1.addLast(3);
+        al1.addLast(4);
+        al1.addLast(5);
+        al1.addLast(6);
+        al1.addLast(7);
+        al1.addLast(8);
+        assertThat(al1.toList()).containsExactly(1,2,3,4,5,6,7,8).inOrder();
+
+        //测试resize顺序表
+        al1.addLast(9);
+        assertThat(al1.toList()).containsExactlyElementsIn(Arrays.asList(1,2,3,4,5,6,7,8,9));
+    }
+
+    @Test
+    public void getTest(){
+        ArrayDeque61B<Integer> al1 = new ArrayDeque61B<>();
+        int i;
+        al1.addFirst(8);
+        al1.addFirst(7);
+        al1.addFirst(6);
+        al1.addFirst(5);
+        al1.addFirst(4);
+        al1.addFirst(3);
+        al1.addFirst(2);
+        al1.addFirst(1);
+        for (i=0; i < al1.size(); i++){
+            System.out.println(al1.get(i));
+        }
+        al1.addFirst(0);
+        for (i=0; i < al1.size(); i++){
+            System.out.println(al1.get(i));
+        }
+    }
+
+    @Test
+    public void removeFirstTest(){
+        ArrayDeque61B<Integer> al1 = new ArrayDeque61B<>();
+        al1.addLast(1);
+        al1.addLast(2);
+        al1.addLast(3);
+        al1.addFirst(5);
+        //remove a single one works.
+        al1.removeFirst();
+        assertThat(al1.toList()).containsExactly(1, 2, 3).inOrder();
+
+        //removeFirst all to empty works
+        al1.removeFirst();
+        al1.removeFirst();
+        assertThat(al1.toList()).containsExactly(3).inOrder();
+        //removeFirst works for last one.
+        al1.removeFirst();
+        assertThat(al1.toList()).isEmpty();
+
+    }
+
+    @Test
+    public void removeLastTest(){
+        ArrayDeque61B<Integer> al1 = new ArrayDeque61B<>();
+        al1.addFirst(1);
+        al1.addFirst(2);
+        al1.addFirst(3);
+        al1.addLast(99);
+        assertThat(al1.toList()).containsExactly(3, 2, 1, 99).inOrder();
+        al1.removeLast();
+        assertThat(al1.toList()).containsExactly(3, 2, 1).inOrder();
+    }
+
+    @Test
+    public void toListTest(){
+        ArrayDeque61B<Integer> al1 = new ArrayDeque61B<>();
+        al1.addFirst(1);   //1
+        al1.addLast(2);    //1，2
+        al1.addFirst(3);   //3，1，2
+        al1.addLast(4);    //3，1，2，4
+        assertThat(al1.toList()).containsExactly(3,1,2,4).inOrder();
+
+        al1.removeFirst();   //1，2，4
+        al1.addLast(8);    //1，2，4，8
+        assertThat(al1.toList()).containsExactly(1, 2, 4, 8).inOrder();
+
+        al1.removeLast();    //1，2，4
+        al1.addFirst(679); //679，1，2，4
+        assertThat(al1.toList()).containsExactly(679, 1, 2, 4).inOrder();
+    }
+
+    @Test
+    public void isEmptyTest(){
+        ArrayDeque61B<Integer> al1 = new ArrayDeque61B<>();
+        assertThat(al1.toList()).isEmpty();
+
+        al1.addFirst(1);
+        assertThat(al1.toList()).isNotEmpty();
+
+        al1.removeLast();
+        assertThat(al1.toList()).isEmpty();
+    }
+
+    @Test
+    public void sizeTest(){
+        ArrayDeque61B<Integer> al1 = new ArrayDeque61B<>();
+        assertThat(al1.size()).isEqualTo(0);
+
+        al1.addFirst(1);
+        assertThat(al1.size()).isEqualTo(1);
+        al1.addLast(2);
+        assertThat(al1.size()).isEqualTo(2);
+        al1.removeLast();
+        assertThat(al1.size()).isEqualTo(1);
+        al1.removeFirst();
+        assertThat(al1.size()).isEqualTo(0);
+
+        al1.addFirst(888);
+        al1.addFirst(777);
+        al1.addFirst(666);
+        al1.addFirst(555);
+        al1.addFirst(444);
+        al1.addFirst(333);
+        al1.addFirst(222);
+        al1.addFirst(111);
+        assertThat(al1.size()).isEqualTo(8);
+
+        al1.addFirst(1000);
+        al1.addFirst(10000);
+        assertThat(al1.size()).isEqualTo(10);
+    }
 }
